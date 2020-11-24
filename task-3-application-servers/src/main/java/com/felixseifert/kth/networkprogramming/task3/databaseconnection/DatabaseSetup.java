e73344f09c7a1c589589805d5c5f4afb7c220276
Sanskar Gupta <Sanskar95@users.noreply.github.com>
2020-11-24 17:04:07 +0100

    reverting to h2 driver

--- task-3-application-servers/src/main/java/com/felixseifert/kth/networkprogramming/task3/databaseconnection/DatabaseSetup.java
+++ task-3-application-servers/src/main/java/com/felixseifert/kth/networkprogramming/task3/databaseconnection/DatabaseSetup.java
@@ -16,7 +16,7 @@ public class DatabaseSetup implements ServletContextListener {
     public void contextInitialized(ServletContextEvent sce) {
 
         try {
-            Class.forName("org.postgresql.Driver");
+            Class.forName("org.h2.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
